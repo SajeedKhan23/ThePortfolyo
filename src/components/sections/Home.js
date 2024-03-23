aa03295cebf532813f500d0468eead40728e4347
Sajeed Khan <100460706+SajeedKhan23@users.noreply.github.com>
2024-03-23 23:23:49 +0530

    Update Home.js
    Updated the fetch API address.

--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   const [email, setEmail] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/userData')
+    fetch('https://the-portfolyo-iota.vercel.app/api/userData')
     .then(response => response.json())
     .then(data => {
       setUserName(data.about.name);
